71ab359eef5fefb2f569e224d25352a5387596b2
Sebastian Uribe <suribe@gmail.com>
2020-04-17 11:55:41 +0200

    Add The Political Beliefs of Rocks

---
+++ rocks/js/rockdata.js
@@ -0,0 +1,158 @@
+const rocks = [
+  {
+    imageurl: 'img/rocks_01.jpg',
+    left: {
+      english: 'This rocks thinks that excessive mineralization is the biggest risks rocks face these days, and whoever thinks differently is an ignorant idiot.',
+      german: 'Dieser Stein denkt, dass Übermineralisierung das größte Risiko für Steine ist, und wer anders denkt ist ein(e) Idiot(in)'
+    },
+    right : {
+      english: 'This rock thinks that insufficient mineralization threatens rocks more than anything else, and those who disagree are pedantic morons',
+      german: 'Dieser Stein denkt, dass Untermineralisierung Steine vor allem gefährdet, und Steine die anderer Meinung sind, sind pedantische Dummköpfe'
+    }
+  },
+  {
+    imageurl: 'img/rocks_02.jpg',
+    left: {
+      english: 'This rocks is not concerned about sea levels rising and covering it, because „many other rocks will be underwater before it happens to me“',
+      german: 'Dieser Stein ist nicht besorgt, dass der Meeresspiegel ansteigt und ihn bedeckt, denn "viele andere Steine werden unter Wasser sein, bevor es mir passiert"'
+    },
+    right : {
+      english: 'This rock decided not to care about extreme weather events, as it has never seen any rock affected by them.',
+      german: 'Dieser Stein macht sich keine Sorgen um extreme Wetterereignisse, da es keine Steine kennt, die davon beschädigt worden sind.'
+    }
+  },
+  {
+    imageurl: 'img/rocks_03.jpg',
+    left: {
+      english: 'This rock does not believe that small rocks can have opinions about politics, they should leave it to bigger rocks',
+      german: 'Dieser Stein schätzt die politische Meinung von großen Steine höher als die von kleinen, weil kleine Steine „keine Ahnung“ von Politik haben.'
+    },
+    right : {
+      english: 'This rock does not like to talk about politics because anyway "things are the way they are and people just have to deal with it“',
+      german: 'Dieser Stein diskutiert nicht über Politik, denn: "Ohnehin können wir nichts ändern."'
+    }
+  },
+  {
+    imageurl: 'img/rocks_04.jpg',
+    left: {
+      english: 'This rock chooses politicians who are willing to place the blame for their problems on those with different color or mineral structure.',
+      german: 'Dieser Stein wählt die Politiker, die für ihre Probleme die Steine mit anderer Farbe oder Mineralstruktur verantwortlich machen.'
+    },
+    right : {
+      english: 'This rock wonders what will happen when rocks are not needed anymore, and whether they will receive some kind of universal basic income',
+      german: 'Der Stein fragt sich, was mit Steinen geschehen wird, wenn sie nicht meht benötig werden, und ob sie ein Bedingungsloses Grundeinkommen bekommen werden.'
+    }
+  },
+  {
+    imageurl: 'img/rocks_05.jpg',
+    left: {
+      english: 'This rock believes that the trickle down effect will eventually bring minerals to all rocks, so it does not matter if some start with more than others',
+      german: 'Dieser Stein glaubt, dass der "trickle down"-Effekt eventuell allen Steine Mineralien bringen wird. Dann ist es unwichtig, ob einige mit mehr Mineralien als andere anfangen.'
+    },
+    right : {
+      english: 'This rock thinks that only useful rocks or those worth a lot should be allowed to enter their territory and stay there',
+      german: 'Der Stein glaubt, dass es nur hochwertigen oder wertvollen Steine erlaubt sein sollte, in das Land einzuwandern, um zu bleiben.'
+    }
+  },
+  {
+    imageurl: 'img/rocks_06.jpg',
+    left: {
+      english: 'These rocks repeat most opinions that they hear, especially if they think that the other rocks will agree with them',
+      german: 'Diese Steine wiederholen alles, was sie hören, besonders wenn sie denken, dass die anderen es auch wiederholen werden.'
+    },
+    right : {
+      english: 'This rock thinks that even if democracy is not perfect, it\'s the best system out there. It does not know how to define democracy with precision, though.',
+      german: 'Der Stein denkt, dass Demokratie das beste System ist, wenn auch nicht perfekt. Er kann aber nicht genau erklären, was Demokratie ist.'
+    }
+  },
+];
+
+const portraits = [
+  {
+    imageurl: 'img/portraits_05.jpg',
+    title: 'The White Rock Above us All',
+    text: `A founding myth that still influences Rock society to this day, is that of the <i>White Rock above us all</i>.<br>
+    Rocks place great emphasis on their vertical placement. A higher physical position is regarded also as high socially, and this seems to influence all of rocks’ activities and opinions. It is no surprise that the periodic sighting of what looks like a white, round rock floating above everything else, has exerted a strong influence on the perception of superiority in countless generations of rocks everywhere. What is surprising, though, is that rocks seem to have arbitrarily focused on the color of this rock, and not in other factors - like its shape, or the periodicity with which it appears - and used it to proclaim the superiority of white rocks above all others.<br>
+    Even when there is no scientific evidence to support this claim of superiority, and when plenty of rocks these days disagree with this perceived superiority, its effects can still be seen everywhere in rock society.`,
+    reference: 'Origins of Rock culture: legends and folk tales, B. Neu'
+  },
+  {
+    imageurl: 'img/portraits_02.jpg',
+    title: 'The UrRock and the diminishing power of age',
+    text: `In the beginning was the rock. A gigantic rock, the size of the world. A rock containing all ideas and thoughts that there have even been and will ever be. Then, a meteorite hit broke it up, diving both mind and matter. These pieces eventually broke up too, and the more the rocks subdivided, the more their ideas were spread apart. This is the main foundational myth of rocks: the UrRock.<br>
+    It’s possible to understand a lot about rocks from it. Young, big rocks contain many thoughts and ideas. They can communicate easily with others, as they will always find some similar thoughts that, even if not perfectly shared, are close enough to allow mutual understanding. As rocks age and reduce both their size and mind, they become focused on a very small set of things. Incapable of sharing ideas, understanding others, or being understood, they end up secluded. Becoming smaller and smaller, they dissolve not only physicaly but also socially, eventually reaching the point where they are never to be seen again.`,
+    reference: 'Origins of Rock culture: legends and folk tales,  B. Neu'
+  },
+  {
+    imageurl: 'img/portraits_01.jpg',
+    title: 'Time and the Perception of Change',
+    text: `The inherent longevity of rocks gives them a very different perspective on time, than that of humans. But while it might be initially understood as simply a matter of scale, or in other words, that they perceive things for longer or slower, their perception is actually of a different kind altogether.<br>
+    Rocks don’t change, unless affected by external factors. Does this immutability also apply to their perceptive capabilities? Can perception exist without mutation? Rocks do perceive change, they just have a hard time accepting it. In a way, they are incapacitated to incorporate this sensing. As this rock once famously said: <i>“Rocks can see, but not know”</i>.<br>
+    For example, a rock might sense an increase in temperature through time, but if it believes that temperature does not change, then it will not change its mind. Or worse: it might even say that the change happens backwards, and that the temperature is actually declining! Even when faced with external evidence, it might deny it and become fixated in its view that this change is not happening, and it might be impossible to change its mind.`,
+    reference: 'Thinking with Rocks, Vol. 2, B. Neu'
+  },
+  {
+    imageurl: 'img/portraits_04.jpg',
+    title: 'Rock Art',
+    text: `While it is clear that handling a musical instrument or a paintbrush would be a challenging task for a rock, there is no real reason for them to not engage in other forms of art, like singing, or oral storytelling. One could expect that such long-lived entities would be particularly apt at long prose, and epic poems should came natural to them. Yet, art is rare among rocks.<br>
+    Their best known poem (anonymous):<br>
+    <p class="i f3">
+    I am a rock, solid rock.<br>
+    There are other,<br>
+    &nbsp;&nbsp;rocks, too.<br>
+    We are rocks, a rock<br>
+    A rock, a rock rock rock.</p>`,
+    reference: 'Origins of Rock culture: legends and folk tales, B. Neu'
+  },
+  {
+    imageurl: 'img/portraits_03.jpg',
+    title: 'Evolutionary efficiency',
+    text: `Do rocks move? After several years spent among rocks, conducting numerous field experiments and measurements, I can say with a great degree of certainty: no.<br>
+    Most rocks don’t even question it: as far as they know it has always been like that, and they couldn’t care less if it remained that way forever. While it’s true that they don’t engage in hunting or gathering activities, nor they need to escape from predators, it might still be useful for them to be able to change where they stand once in a while. Visit places. See different landscapes. Still, they don’t. But Why?<br>
+    A rock over which I was lying on a sunny provided me with a clue to this mystery. The evolutionary process continuously improves the fitness of an organism to their environment, which mostly translates to being able to process energy efficiently. And who is more efficient than rocks? They can accumulate energy through heat or magnetism, and make very little use of it. And they have remained in their current form for as long as anyone knows, unaltered by the evolutionary process. Might rocks be the culmination of evolution?`,
+    reference: 'Thinking with Rocks, Vol. 1, B. Neu'
+  },
+];
+
+function writeRocks() {
+  for (var index = 0 ; index < rocks.length ; index++) {
+    var data = rocks[index];
+    const template = `<article class="rock-article cf">
+  <img class="rock-image" src="${data.imageurl}">
+  <div class="fl w-50 ph4 pv2">
+    <div class="label pv1 ph3">
+      <p class="english f4 courier">${data.left.english}</p>
+      <p class="german f4 courier">${data.left.german}</p>
+    </div>
+  </div>
+  <div class="fl w-50 ph4 pv2">
+    <div class="label pv1 ph3">
+      <p class="english f4 courier">${data.right.english}</p>
+      <p class="german f4 courier">${data.right.german}</p>
+    </div>
+  </div>
+  </article>
+  <br>
+  <br>`;
+  document.write(template);
+  }
+}
+
+function writePortraits() {
+  for (var index = 0 ; index < portraits.length ; index++) {
+    var data = portraits[index];
+    const template = `<article class="portrait-article flex w-100 flex-wrap">
+    <div class="w-50-l">
+      <img class="portrait-image mv2" src="${data.imageurl}">
+    </div>
+    <div class="portrait-plaque w-50-l ph5 pv3">
+      <p class="f2 serif pv0 mv0">${data.title}</p>
+      <p class="serif f4 lh-copy tj">${data.text}</p>
+      <p class="serif f5 tr i">- ${data.reference}</p>
+    </div>
+  </article>
+  <br>
+  <br>`;
+  document.write(template);
+  }
+}
